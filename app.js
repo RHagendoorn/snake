9228bdcc9a30bae8db1279e811358233319945ed
Robbert Hagendoorn <robberthagendoorn@geodan.com>
2018-07-09 21:37:12 +0200

    clean up code

--- app.js
+++ app.js
@@ -5,46 +5,47 @@ window.onload = () => {
 	const ctx = canvas.getContext('2d');
 
 	const size = 20;
-	const length = {l: 5};
-	const tail = [];
-	const loc = {x: 200, y: 200};
-	const dir= {x: size, y: 0};
-	const food = {
-		x: Math.floor(Math.random() * canvas.width / size) * size,
-		y: Math.floor(Math.random() * canvas.height / size) * size
+
+	const model = {
+		size: size,
+		length: 5,
+		tail: [],
+		loc: {x: canvas.width / 2, y: canvas.height / 2},
+		dir: {x: size, y: 0},
+		foodLoc: getFoodLoc(canvas, size)
 	};
 
-	document.addEventListener('keydown', event => changeDirection(event, dir, size));
-	window.setInterval(drawGame, 100, canvas, ctx, loc, length, dir, size, tail, food);
+	document.addEventListener('keydown', event => changeDirection(event, model));
+	window.setInterval(drawGame, 100, {canvas, ctx, model});
 };
 
-const changeDirection = (event, dir, size) => {
+const changeDirection = (event, model) => {
 	switch(event.keyCode) {
 		case 37:
-			dir.x = dir.x === size ? size : -size;
-			dir.y = 0;
+			model.dir.x = model.dir.x === model.size ? model.size : -model.size;
+			model.dir.y = 0;
 			break;
 		case 38:
-			dir.x = 0;
-			dir.y = dir.y === size ? size : -size;
+			model.dir.x = 0;
+			model.dir.y = model.dir.y === model.size ? model.size : -model.size;
 			break;
 		case 39:
-			dir.x = dir.x === -size ? -size : size;
-			dir.y = 0;
+			model.dir.x = model.dir.x === -model.size ? -model.size : model.size;
+			model.dir.y = 0;
 			break;
 		case 40:
-			dir.x = 0;
-			dir.y = dir.y === -size ? -size : size;
+			model.dir.x = 0;
+			model.dir.y = model.dir.y === -model.size ? -model.size : model.size;
 	}
 };
 
-const drawGame = (canvas, ctx, loc, length, dir, size, tail, food)  => {
+const drawGame = ({canvas, ctx, model})  => {
 	drawField(ctx, canvas);
-	updateLocation(loc, tail, dir, length);
-	checkLose(tail, length, loc, dir, size);
-	drawFood(loc, food, length, canvas, ctx, size);
-	jumpOver(canvas, loc, size);
-	drawSnake(ctx, loc, tail, size, length);
+	updateLocation(model);
+	checkLose(canvas, model);
+	drawFood({ctx, canvas, model});
+	jumpOver(canvas, model);
+	drawSnake(ctx, model);
 };
 
 const drawField = (ctx, canvas) => {
@@ -55,65 +56,71 @@ const drawField = (ctx, canvas) => {
 	ctx.strokeRect(0, 0, canvas.height, canvas.width);
 };
 
-const updateLocation = (loc, tail, dir, length) => {
-	loc.x += dir.x;
-  	loc.y += dir.y;
+const updateLocation = (model) => {
+	model.loc.x += model.dir.x;
+  	model.loc.y += model.dir.y;
 };
 
-const jumpOver = (canvas, loc, size) => {
+const jumpOver = (canvas, model) => {
 	switch(true) {
-		case loc.x === canvas.width:
-			loc.x = 0;
+		case model.loc.x === canvas.width:
+			model.loc.x = 0;
 			break;
-		case loc.x < 0:
-			loc.x = canvas.width - size;
+		case model.loc.x < 0:
+			model.loc.x = canvas.width - model.size;
 			break;
-		case loc.y === canvas.height:
-			loc.y = 0;
+		case model.loc.y === canvas.height:
+			model.loc.y = 0;
 			break;
-		case loc.y < 0:
-			loc.y = canvas.height - size;
+		case model.loc.y < 0:
+			model.loc.y = canvas.height - model.size;
 			break;
 		default:
 			break;
 	}
 };
 
-const drawSnake = (ctx, loc, tail, size, length) => {
+const drawSnake = (ctx, model) => {
 	ctx.fillStyle = 'black';
-	ctx.fillRect(loc.x, loc.y, size, size);
-	tail.push({x: loc.x, y: loc.y});
-	reduceTail(tail, length);
+	ctx.fillRect(model.loc.x, model.loc.y, model.size, model.size);
+	const tail = [...model.tail, {x: model.loc.x, y: model.loc.y}];
+	model.tail = reduceTail(tail, model.length);
 	tail.forEach(elem => {
-		ctx.fillRect(elem.x, elem.y, size, size)
+		ctx.fillRect(elem.x, elem.y, model.size, model.size)
 	});
 };
 
 const reduceTail = (tail, length) => {
-	if (tail.length > length.l) {
-		tail.shift();
-		reduceTail(tail, length);
+	if (tail.length > length) {
+		return reduceTail(tail.slice(1), length);
+	}
+	return tail;
+};
+
+const checkLose = (canvas, model) => {
+	if (model.tail.some(elem => elem.x === model.loc.x && elem.y === model.loc.y)) {
+		model.length = 5;
+		model.loc.x = canvas.width / 2;
+		model.loc.y = canvas.height / 2;
+		model.dir.x = model.size;
+		model.dir.y = 0;
 	}
 };
 
-const checkLose = (tail, length, loc, dir, size) => {
-	if (tail.some(elem => elem.x === loc.x && elem.y === loc.y)) {
-		length.l = 5;
-		loc.x = 200;
-		loc.y = 200;
-		dir.x = size;
-		dir.y = 0;
-	}
+const getFoodLoc = (canvas, size) => {
+	return {
+		x: Math.floor(Math.random() * canvas.width / size) * size,
+		y: Math.floor(Math.random() * canvas.height / size) * size
+	};
 };
 
-const drawFood = (loc, food, length, canvas, ctx, size) => {
-	if (loc.x === food.x && loc.y === food.y) {
-		length.l += 1;
-		food.x = Math.floor(Math.random() * canvas.width / size) * size;
-		food.y = Math.floor(Math.random() * canvas.height / size) * size;
+const drawFood = ({ctx, canvas, model}) => {
+	if (model.loc.x === model.foodLoc.x && model.loc.y === model.foodLoc.y) {
+		model.length += 1;
+		model.foodLoc = getFoodLoc(canvas, model.size);
 	}
 
 	ctx.fillStyle = 'red';
-	ctx.fillRect(food.x, food.y, size, size)
+	ctx.fillRect(model.foodLoc.x, model.foodLoc.y, model.size, model.size)
 };
 
